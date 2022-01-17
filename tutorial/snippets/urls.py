6a49d444c75fde3e83dfecdc553c4db6c36ee30f
sonuthoughtwin <sonu.thoughtwin@gmail.com>
2022-01-17 13:40:19 +0530

    rest framwork api crud

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+from snippets.models import Snippet
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
